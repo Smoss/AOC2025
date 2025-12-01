d1d39a3193e5352b8820bd5ea0b71b00da09add9
Sean Moss <smoss117@gmail.com>
2025-11-30 22:42:45 -0800

    Day 1 Cleanup

--- Day1/Day1.go
+++ Day1/Day1.go
@@ -16,41 +16,29 @@ func main() {
 	for _, twist := range twists {
 		dir := twist[0:1]
 		quantity, _ := strconv.Atoi(strings.Trim(twist[1:], " \r\n"))
-
+		orig := curr_point
 		switch dir {
 		case "L":
-			for quantity > 0 {
-				curr_point--
-				quantity -= 1
-				if curr_point == 0 {
-					true_crossings++
-				}
-				if curr_point < 0 {
-					curr_point = 99
-				}
-			}
+			curr_point -= quantity
 		default:
-			for quantity > 0 {
-				curr_point++
-				quantity -= 1
-				if curr_point >= 100 {
-					curr_point = 0
-					true_crossings++
-				}
-			}
+			curr_point += quantity
 		}
+		true_crossings += (quantity / 100)
 		for curr_point < 0 {
 			curr_point = curr_point + 100
 		}
 		for curr_point >= 100 {
 			curr_point = curr_point - 100
 		}
-
+		if dir == "L" && curr_point > orig && orig != 0 {
+			true_crossings += 1
+		} else if dir == "R" && curr_point < orig && curr_point != 0 {
+			true_crossings += 1
+		}
 		if curr_point == 0 {
 			crossings += 1
+			true_crossings++
 		}
-		fmt.Printf("Curr dial: %d, %d, %s, %d", quantity, curr_point, twist[:1], true_crossings)
-		fmt.Println()
 	}
 	fmt.Printf("%d, %d", crossings, true_crossings)
 	fmt.Println()
